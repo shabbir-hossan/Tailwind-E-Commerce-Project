44e524175b83678eab515faa15988c5394e44ed4
shabbir-hossan <afrankhanshabbir@gmail.com>
2024-04-16 18:24:37 +0600

    Tailwind e-com project container line

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.{html,js}'],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins, sans-serif',
+        roboto: 'Roboto, sans-serif',
+      },
+    },
+  },
+  plugins: [],
+};
